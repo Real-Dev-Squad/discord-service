09052be4bf1544c2a497a946433cf0dbce3fbbc2
Joy <joygupta501@gmail.com>
2024-10-12 23:24:48 +0530

    Feat : Utilised custom error handler

--- controllers/baseHandler.go
+++ controllers/baseHandler.go
@@ -6,24 +6,22 @@ import (
 	"net/http"
 
 	"github.com/Real-Dev-Squad/discord-service/dtos"
+	"github.com/Real-Dev-Squad/discord-service/utils"
 	"github.com/bwmarrin/discordgo"
 	"github.com/julienschmidt/httprouter"
-	"github.com/sirupsen/logrus"
 )
 
 func HomeHandler(response http.ResponseWriter, request *http.Request, params httprouter.Params) {
 	response.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	payload, err := io.ReadAll(request.Body)
 	if err != nil {
-		logrus.Error(err)
-		http.Error(response, "Failed to read request body", http.StatusBadRequest)
+		utils.Errors.NewBadRequestError(response, "Failed to read request body")
 		return
 	}
 	var message dtos.DiscordMessage
 	err = json.Unmarshal(payload, &message)
 	if err != nil {
-		logrus.Error(err)
-		http.Error(response, "Failed to parse request body", http.StatusBadRequest)
+		utils.Errors.NewInternalError(response)
 		return
 	}
 	switch message.Type {
@@ -31,8 +29,7 @@ func HomeHandler(response http.ResponseWriter, request *http.Request, params htt
 		resp := map[string]uint8{"type": uint8(discordgo.InteractionResponsePong)}
 		err = json.NewEncoder(response).Encode(resp)
 		if err != nil {
-			logrus.Error(err)
-			http.Error(response, "Internal Server Error", http.StatusInternalServerError)
+			utils.Errors.NewInternalError(response)
 			return
 		}
 	default:

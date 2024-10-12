74423d890a54777524d1ab78ef375d2f5c787806
Joy <joygupta501@gmail.com>
2024-10-12 23:20:55 +0530

    Feat : Added custom error handler

--- middleware/verifyCommand.go
+++ middleware/verifyCommand.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Real-Dev-Squad/discord-service/config"
+	"github.com/Real-Dev-Squad/discord-service/utils"
 	"github.com/bwmarrin/discordgo"
 	"github.com/julienschmidt/httprouter"
 )
@@ -13,12 +14,12 @@ func VerifyCommand(next httprouter.Handle) httprouter.Handle {
 	return func(response http.ResponseWriter, request *http.Request, params httprouter.Params) {
 		publicKeyBytes, err := hex.DecodeString(config.AppConfig.DISCORD_PUBLIC_KEY)
 		if err != nil {
-			response.WriteHeader(http.StatusInternalServerError)
+			utils.Errors.NewInternalError(response)
 			return
 		}
 		result := discordgo.VerifyInteraction(request, publicKeyBytes)
 		if !result {
-			response.WriteHeader(http.StatusUnauthorized)
+			utils.Errors.NewUnauthorisedError(response)
 			return
 		}
 		next(response, request, params)

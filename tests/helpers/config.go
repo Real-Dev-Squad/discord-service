8df1402f9d72c90ce434f2037710e86f43c0d8e2
Joy <joygupta501@gmail.com>
2024-10-13 14:57:22 +0530

    Chore : Fixed DISCORD_PUBLIC_KEY for test

--- tests/helpers/config.go
+++ tests/helpers/config.go
@@ -5,5 +5,5 @@ import "os"
 func init() {
 	os.Setenv("MODE", "test")
 	os.Setenv("PORT", "8080")
-	os.Setenv("DISCORD_PUBLIC_KEY", "<DISCORD_PUBLIC_KEY>")
+	os.Setenv("DISCORD_PUBLIC_KEY", "8933e3749b4feb4d76169b26ed372af3c378f4353c2024fee0601f2a2e7918e1")
 }

4b3181af4e44c7f04002eca3858904bfbb3d9319
Joy <joygupta501@gmail.com>
2024-10-20 16:01:12 +0530

    Test : Updated config helper

--- tests/helpers/config.go
+++ tests/helpers/config.go
@@ -1,9 +1,13 @@
 package helpers
 
-import "os"
+import (
+	"os"
+)
 
 func init() {
 	os.Setenv("MODE", "test")
 	os.Setenv("PORT", "8080")
 	os.Setenv("DISCORD_PUBLIC_KEY", "8933e3749b4feb4d76169b26ed372af3c378f4353c2024fee0601f2a2e7918e1")
+	os.Setenv("GUILD_ID", "8933e3749b4feb4d76169b26ed372af3c378f4353c2024fee0601f2a2e7918e1")
+	os.Setenv("BOT_TOKEN", "8933e3749b4feb4d76169b26ed372af3c378f4353c2024fee0601f2a2e7918e1")
 }

6de10f57f53f6c9b26b00b8d323fe32c823249a6
Joy <56365512+joyguptaa@users.noreply.github.com>
2024-11-05 22:53:50 +0530

    Hello command (#19)
    * Feat : Added hello command interaction endpoint
    
    * Feat : Added test for basehandler
    
    * Refactor : Code cleanup

--- controllers/baseHandler.go
+++ controllers/baseHandler.go
@@ -28,6 +28,15 @@ func HomeHandler(response http.ResponseWriter, request *http.Request, params htt
 		payload := map[string]interface{}{"type": uint8(discordgo.InteractionResponsePong)}
 		utils.Success.NewDiscordResponse(response, "Pong", payload)
 		return
+	case discordgo.InteractionApplicationCommand:
+		messageResponse := &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Hey there! Congratulations, you just executed your first slash command",
+			},
+		}
+		utils.Success.NewDiscordResponse(response, "Success", messageResponse)
+		return
 	default:
 		response.WriteHeader(http.StatusOK)
 	}

9f9cff6ab2a517bb895f74726b65faf77e17b976
Joy <joygupta501@gmail.com>
2025-01-23 23:01:49 +0530

    Test : Updated Test

--- queue/main_test.go
+++ queue/main_test.go
@@ -13,53 +13,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockSession struct {
+type mockQueue struct {
 	dialError    error
 	channelError error
 	queueError   error
 }
 
-func (m *mockSession) dial() error {
+func (m *mockQueue) dial() error {
 	return m.dialError
 }
 
-func (m *mockSession) createChannel() error {
+func (m *mockQueue) createChannel() error {
 	return m.channelError
 }
-func (m *mockSession) declareQueue() error {
+func (m *mockQueue) declareQueue() error {
 	return m.queueError
 }
 
 func TestInitQueueConnection(t *testing.T) {
 	config.AppConfig.MAX_RETRIES = 1
 	t.Run("should not panic when Dial() returns error", func(t *testing.T) {
-		mockSess := &mockSession{dialError: errors.New("connection failed")}
+		mockQueue := &mockQueue{dialError: errors.New("connection failed")}
 		assert.NotPanics(t, func() {
-			InitQueueConnection(mockSess)
+			InitQueueConnection(mockQueue)
 		}, "InitQueueConnection should not panic when Dial is unsuccessful")
 
 	})
 
 	t.Run("should not panic when CreateChannel() returns error", func(t *testing.T) {
-		mockSess := &mockSession{channelError: errors.New("channel failed")}
+		mockQueue := &mockQueue{channelError: errors.New("channel failed")}
 		assert.NotPanics(t, func() {
-			InitQueueConnection(mockSess)
+			InitQueueConnection(mockQueue)
 		}, "InitQueueConnection should not panic when CreateChannel is unsuccessful")
 
 	})
 
 	t.Run("should not panic when DeclareQueue() returns error", func(t *testing.T) {
-		mockSess := &mockSession{queueError: errors.New("queue failed")}
+		mockQueue := &mockQueue{queueError: errors.New("queue failed")}
 		assert.NotPanics(t, func() {
-			InitQueueConnection(mockSess)
+			InitQueueConnection(mockQueue)
 		}, "InitQueueConnection should not when DeclareQueue is unsuccessful")
 
 	})
 
 	t.Run("should pass when no error is returned", func(t *testing.T) {
-		mockSess := &mockSession{}
+		mockQueue := &mockQueue{}
 		assert.NotPanics(t, func() {
-			InitQueueConnection(mockSess)
+			InitQueueConnection(mockQueue)
 		})
 	})
 }
@@ -67,12 +67,12 @@ func TestInitQueueConnection(t *testing.T) {
 func TestGetQueueInstance(t *testing.T) {
 	t.Run("Should use ExponentialBackoffRetry via GetQueueInstance", func(t *testing.T) {
 		attempt := 0
-		originalDef := utils.ExponentialBackoffRetry
+		originalFunc := utils.ExponentialBackoffRetry
 		utils.ExponentialBackoffRetry = func(maxRetries int, operation func() error) error {
 			attempt++
 			return errors.New("error")
 		}
-		defer func() { utils.ExponentialBackoffRetry = originalDef }()
+		defer func() { utils.ExponentialBackoffRetry = originalFunc }()
 		assert.Nil(t, GetQueueInstance())
 		assert.Equal(t, 1, attempt)
 	})
@@ -109,28 +109,6 @@ func TestSendMessage(t *testing.T) {
 		message := dtos.TextMessage{
 			Text:     "test",
 			Priority: 1,
-		}
-		assert.Panics(t, func() {
-			SendMessage(message)
-		}, "SendMessage should panic when SendMessage returns error")
-	})
-
-	t.Run("Should panic when SendMessage returns error", func(t *testing.T) {
-		config.AppConfig.MAX_RETRIES = 1
-		message := dtos.TextMessage{
-			Text:     "test",
-			Priority: 1,
-		}
-		mockQueueSession := &Queue{
-			Connection: &amqp.Connection{},
-			Name:       "Testing",
-			Channel:    &amqp.Channel{},
-			Queue:      amqp.Queue{},
-		}
-		originalFunc := GetQueueInstance
-		defer func() { GetQueueInstance = originalFunc }()
-		GetQueueInstance = func() *Queue {
-			return mockQueueSession
 		}
 		assert.Panics(t, func() {
 			SendMessage(message)

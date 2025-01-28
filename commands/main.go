d1877bea1a664853a6b8ec981222436cbc50fefb
Joy <joygupta501@gmail.com>
2025-01-28 21:36:33 +0530

    Feat : Added listening command

--- commands/main.go
+++ commands/main.go
@@ -7,4 +7,16 @@ var Commands = []*discordgo.ApplicationCommand{
 		Name:        "hello",
 		Description: "Greets back with hello!",
 	},
+	{
+		Name:        "listening",
+		Description: "mark user as listening",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "value",
+				Description: "to enable or disable the listening mode",
+				Type:        5,
+				Required:    true,
+			},
+		},
+	},
 }

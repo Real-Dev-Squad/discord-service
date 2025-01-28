5472f0288d8bcf1c143cd6c0f6ed64554d770ae7
Joy <joygupta501@gmail.com>
2025-01-28 21:40:32 +0530

    Feat : Added helper to DataPacket

--- dtos/queue.go
+++ dtos/queue.go
@@ -1,6 +1,31 @@
 package dtos
 
-type TextMessage struct {
-	Text     string
-	Priority uint8
+import (
+	"encoding/json"
+
+	"github.com/sirupsen/logrus"
+)
+
+type DataPacket struct {
+	UserID      string
+	CommandName string
+	metaData    map[string]string
+}
+
+func (d *DataPacket) toByte() ([]byte, error) {
+	bytes, err := json.Marshal(d)
+	if err != nil {
+		logrus.Errorf("Failed to marshal message: %v", err)
+		return nil, err
+	}
+	return bytes, nil
+}
+
+func (d *DataPacket) fromByte(bytes []byte) error {
+	err := json.Unmarshal(bytes, d)
+	if err != nil {
+		logrus.Errorf("Failed to unmarshal message: %v", err)
+		return err
+	}
+	return nil
 }

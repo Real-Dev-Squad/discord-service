23170c0b7f9e731069e45181a97ad2478e1d2220
Joy <joygupta501@gmail.com>
2025-01-29 01:25:21 +0530

    Test : Added test for commandHandler.MainHandler in QueueHandler

--- controllers/queueHandler_test.go
+++ controllers/queueHandler_test.go
@@ -7,7 +7,9 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/Real-Dev-Squad/discord-service/config"
 	"github.com/Real-Dev-Squad/discord-service/controllers"
+	"github.com/Real-Dev-Squad/discord-service/utils"
 	"github.com/julienschmidt/httprouter"
 	"github.com/stretchr/testify/assert"
 )
@@ -29,6 +31,27 @@ func TestQueueHandler(t *testing.T) {
 		router.ServeHTTP(rr, req)
 		assert.Equal(t, http.StatusOK, rr.Code)
 		assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+	})
+	t.Run("should be able to execute listening command", func(t *testing.T) {
+		body := []byte(`{"CommandName": "listening"}`)
+		req, err := http.NewRequest("POST", "/queue", bytes.NewBuffer(body))
+		assert.NoError(t, err)
+		rr := httptest.NewRecorder()
+		router.ServeHTTP(rr, req)
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+	})
+
+	t.Run("should fail if ExponentialBackoffRetry fails for listening command", func(t *testing.T) {
+		config.AppConfig.MAX_RETRIES = 1
+		originalFunc := utils.ExponentialBackoffRetry
+		utils.ExponentialBackoffRetry = func(maxRetries int, operation func() error) error {
+			return errors.New("error")
+		}
+		defer func() { utils.ExponentialBackoffRetry = originalFunc }()
+		body := []byte(`{"CommandName": "listening"}`)
+		_, err := http.NewRequest("POST", "/queue", bytes.NewBuffer(body))
+		assert.NoError(t, err)
 	})
 
 	t.Run("should return 500 Internal Server Error if payload is unable to be decoded", func(t *testing.T) {

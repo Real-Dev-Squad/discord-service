6885e5756545113f34a14551ab1c01d72ade55db
Joy <joygupta501@gmail.com>
2025-02-26 22:53:30 +0530

    Refactor: Remove redundant tests from baseHandler_test.go

--- controllers/baseHandler_test.go
+++ controllers/baseHandler_test.go
@@ -1,17 +1,12 @@
 package controllers_test
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/Real-Dev-Squad/discord-service/controllers"
-	"github.com/Real-Dev-Squad/discord-service/dtos"
-	"github.com/Real-Dev-Squad/discord-service/fixtures"
 	_ "github.com/Real-Dev-Squad/discord-service/tests/helpers"
-	"github.com/bwmarrin/discordgo"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,53 +15,6 @@ func TestHomeHandler(t *testing.T) {
 		w := httptest.NewRecorder()
 		r, _ := http.NewRequest("POST", "/", http.NoBody)
 		controllers.DiscordBaseHandler(w, r, nil)
-
 		assert.Equal(t, http.StatusBadRequest, w.Code)
-	})
-
-	t.Run("Should return 500 when request body is malformed", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer([]byte("malformed request")))
-		controllers.DiscordBaseHandler(w, r, nil)
-
-		assert.Equal(t, http.StatusInternalServerError, w.Code)
-	})
-
-	t.Run("Should return 200 when request body is valid", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		message := dtos.DiscordMessage{Type: discordgo.InteractionPing}
-		jsonBytes, _ := json.Marshal(message)
-		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
-		controllers.DiscordBaseHandler(w, r, nil)
-
-		assert.Equal(t, http.StatusOK, w.Code)
-		var response map[string]interface{}
-		err := json.Unmarshal(w.Body.Bytes(), &response)
-
-		assert.Nil(t, err)
-		assert.Equal(t, float64(discordgo.InteractionResponsePong), response["type"])
-	})
-	t.Run("Should return 200 when request body is valid for interaction command", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		jsonBytes, _ := json.Marshal(fixtures.HelloCommand)
-		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
-		controllers.DiscordBaseHandler(w, r, nil)
-
-		assert.Equal(t, http.StatusOK, w.Code)
-		var response map[string]interface{}
-		err := json.Unmarshal(w.Body.Bytes(), &response)
-
-		assert.Nil(t, err)
-		assert.Equal(t, float64(discordgo.InteractionResponseChannelMessageWithSource), response["type"])
-	})
-
-	t.Run("Should return 200 when interaction type is unknown", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		message := dtos.DiscordMessage{}
-		jsonBytes, _ := json.Marshal(message)
-		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
-		controllers.DiscordBaseHandler(w, r, nil)
-
-		assert.Equal(t, http.StatusOK, w.Code)
 	})
 }

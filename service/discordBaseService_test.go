2ccae84318957038e819c75f1382e10b60ae4048
Joy <joygupta501@gmail.com>
2025-02-26 22:53:03 +0530

    Test: Add unit tests for DiscordBaseService to validate request handling

---
+++ service/discordBaseService_test.go
@@ -0,0 +1,72 @@
+package service_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Real-Dev-Squad/discord-service/dtos"
+	"github.com/Real-Dev-Squad/discord-service/fixtures"
+	"github.com/Real-Dev-Squad/discord-service/service"
+	_ "github.com/Real-Dev-Squad/discord-service/tests/helpers"
+	"github.com/bwmarrin/discordgo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHomeHandler(t *testing.T) {
+	t.Run("Should return 400 when request body is empty", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("POST", "/", http.NoBody)
+		service.DiscordBaseService(w, r)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("Should return 500 when request body is malformed", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer([]byte("malformed request")))
+		service.DiscordBaseService(w, r)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("Should return 200 when request body is valid", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		message := dtos.DiscordMessage{Type: discordgo.InteractionPing}
+		jsonBytes, _ := json.Marshal(message)
+		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
+		service.DiscordBaseService(w, r)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		var response map[string]interface{}
+		err := json.Unmarshal(w.Body.Bytes(), &response)
+
+		assert.Nil(t, err)
+		assert.Equal(t, float64(discordgo.InteractionResponsePong), response["type"])
+	})
+	t.Run("Should return 200 when request body is valid for interaction command", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		jsonBytes, _ := json.Marshal(fixtures.HelloCommand)
+		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
+		service.DiscordBaseService(w, r)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		var response map[string]interface{}
+		err := json.Unmarshal(w.Body.Bytes(), &response)
+
+		assert.Nil(t, err)
+		assert.Equal(t, float64(discordgo.InteractionResponseChannelMessageWithSource), response["type"])
+	})
+
+	t.Run("Should return 200 when interaction type is unknown", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		message := dtos.DiscordMessage{}
+		jsonBytes, _ := json.Marshal(message)
+		r, _ := http.NewRequest("POST", "/", bytes.NewBuffer(jsonBytes))
+		service.DiscordBaseService(w, r)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+}

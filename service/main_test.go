f7d7a6674b8d6bcef07c4740c425f9ff39db9fdf
Joy <joygupta501@gmail.com>
2025-03-01 09:19:35 +0530

    Refactor: Simplify assertions in TestMainService and adjust MAX_RETRIES configuration

--- service/main_test.go
+++ service/main_test.go
@@ -17,18 +17,26 @@ import (
 )
 
 func TestMainService(t *testing.T) {
+	originalMaxRetries := config.AppConfig.MAX_RETRIES
+	config.AppConfig.MAX_RETRIES = 1
+	defer func() { config.AppConfig.MAX_RETRIES = originalMaxRetries }()
 	t.Run("should return Hello when command name is hello", func(t *testing.T) {
 		CS := CommandService{discordMessage: fixtures.HelloCommand}
 		w := httptest.NewRecorder()
 		r, _ := http.NewRequest("GET", "/", nil)
 		CS.HandleMessage(w, r)
 		assert.Equal(t, http.StatusOK, w.Code)
+		response := discordgo.InteractionResponse{}
+		err := json.Unmarshal(w.Body.Bytes(), &response)
+		assert.NoError(t, err)
+		assert.Equal(t, response.Data.Content, "Hey there \u003c@123456789012345678\u003e! Congratulations, you just executed your first slash command")
+		fmt.Println(w.Body.String())
 	})
 
 	t.Run("should trigger ListeningService when command name is listening", func(t *testing.T) {
 		discordMessage := dtos.DiscordMessage{
 			Member: discordgo.Member{
-				Nick: fmt.Sprintf("test%s", utils.NICKNAME_SUFFIX),
+				Nick: "test" + utils.NICKNAME_SUFFIX,
 			},
 			Data: dtos.Data{
 				GuildId: "876543210987654321",
@@ -48,7 +56,7 @@ func TestMainService(t *testing.T) {
 		messageResponse := discordgo.InteractionResponse{}
 		err := json.Unmarshal(w.Body.Bytes(), &messageResponse)
 		assert.NoError(t, err)
-		assert.Equal(t, messageResponse.Data.Content, "You are already set to listen.")
+		assert.Equal(t, "You are already set to listen.", messageResponse.Data.Content)
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 
@@ -70,7 +78,7 @@ func TestMainService(t *testing.T) {
 				},
 			},
 		}
-		config.AppConfig.MAX_RETRIES = 1
+
 		CS := CommandService{discordMessage: discordMessage}
 		w := httptest.NewRecorder()
 		r, _ := http.NewRequest("GET", "/", nil)
@@ -78,7 +86,7 @@ func TestMainService(t *testing.T) {
 		response := dtos.CommandError{}
 		err := json.Unmarshal(w.Body.Bytes(), &response)
 		assert.NoError(t, err)
-		assert.Equal(t, response.Message, "Queue channel is not initialized")
+		assert.Equal(t, "Queue channel is not initialized", response.Message)
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})
 	t.Run("should trigger VerifyService when command name is verify", func(t *testing.T) {
@@ -98,7 +106,7 @@ func TestMainService(t *testing.T) {
 		messageResponse := discordgo.InteractionResponse{}
 		err := json.Unmarshal(w.Body.Bytes(), &messageResponse)
 		assert.NoError(t, err)
-		assert.Equal(t, messageResponse.Data.Content, "Work in progress for Verify command")
+		assert.Equal(t, "Work in progress for Verify command", messageResponse.Data.Content)
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 

8045d10ebb47f126b1e95cec11103adf53845c6e
Devashish08 <devashish.cs025@gmail.com>
2025-03-25 09:54:26 +0000

    feat: Add mention-each command registration

--- commands/main.go
+++ commands/main.go
@@ -26,4 +26,34 @@ var Commands = []*discordgo.ApplicationCommand{
 		Name:        utils.CommandNames.Verify,
 		Description: "Generate a link with user specific token to link with RDS backend",
 	},
+	{
+		Name: utils.CommandNames.MentionEach,
+		Description: "Mention all users with a specific role",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name: "role",
+				Description: "The role whose members will be mentioned",
+				Type: discordgo.ApplicationCommandOptionRole,
+				Required: true,
+			},
+			{
+				Name: "message",
+				Description: "Custom message to accompany the mentions",
+				Type: discordgo.ApplicationCommandOptionString,
+				Required: false,
+			},
+			{
+				Name: "dev", 
+				Description: "Send individual mentions in separate message",
+				Type: discordgo.ApplicationCommandOptionBoolean,
+				Required: false,
+			},
+			{
+				Name: "dev_title",
+				Description: "Show a list of users with the role withour pinging them",
+				Type: discordgo.ApplicationCommandOptionBoolean,
+				Required: false,
+			},
+		},
+	},
 }

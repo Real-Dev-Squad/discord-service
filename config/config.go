43831f87d4e82d5288eea85022562cec6313ddd6
Devashish08 <devashish.cs025@gmail.com>
2025-04-20 18:56:30 +0000

    feat: add feature flag for mentionEach command

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -19,6 +20,7 @@ type Config struct {
 	MAX_RETRIES           int
 	RDS_BASE_API_URL      string
 	VERIFICATION_SITE_URL string
+	MENTION_EACH_ENABLED  bool
 }
 
 var AppConfig Config
@@ -39,11 +41,13 @@ func init() {
 		QUEUE_NAME:         loadEnv("QUEUE_NAME"),
 		ENV:                Environment(loadEnv("ENV")).Validate(),
 		MAX_RETRIES:        5,
+		MENTION_EACH_ENABLED: loadBoolEnv("MENTION_EACH_ENABLED", false),
 	}
 
 	// Loading Constants
 	AppConfig.RDS_BASE_API_URL = EnvironmentURLs[AppConfig.ENV].RDS_BASE_API_URL
 	AppConfig.VERIFICATION_SITE_URL = EnvironmentURLs[AppConfig.ENV].VERIFICATION_SITE_URL
+	logrus.Infof("Feature Flag Status: MENTION_EACH_ENABLED=%t", AppConfig.MENTION_EACH_ENABLED)
 
 }
 
@@ -53,4 +57,19 @@ func loadEnv(key string) string {
 		logrus.Panic(fmt.Sprintf("Environment variable %s not set", key))
 	}
 	return value
+}
+
+func loadBoolEnv(key string, defaultValue bool) bool {
+	valueStr := os.Getenv(key)
+	if valueStr == "" {
+		logrus.Infof("Environment variable %s not set, defaulting to false", key)
+		return defaultValue
+	}
+	valueBool, err := strconv.ParseBool(valueStr)
+	if err != nil {
+		logrus.Warnf("Invalid boolean value for environment variable %s: '%s'. Defaulting to false.", key, valueStr)
+		return defaultValue
+	}
+	logrus.Infof("Loaded %s=%t", key, valueBool)
+	return valueBool
 }

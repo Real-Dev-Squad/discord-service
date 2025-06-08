1b2920c8bab41feee79a55009f8d5a4d16d3865b
Pankaj <pankajshadev@gmail.com>
2025-06-08 17:11:02 +0530

    feat(verify): Implement asynchronous verification flow

---
+++ service/verify.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Real-Dev-Squad/discord-service/dtos"
+	"github.com/Real-Dev-Squad/discord-service/queue"
+	"github.com/Real-Dev-Squad/discord-service/utils"
+	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
+)
+
+func (s *CommandService) Verify(response http.ResponseWriter, request *http.Request) {
+	dev := "false"
+	if len(s.discordMessage.Data.Options) == 1 && s.discordMessage.Data.Options[0].Value.(bool) {
+		dev = "true"
+	}
+
+	dp := &dtos.DataPacket{
+		UserID:      s.discordMessage.Member.User.ID,
+		CommandName: utils.CommandNames.Verify,
+		MetaData:    map[string]string{
+			"userAvatarHash": s.discordMessage.Member.Avatar,
+			"userName": s.discordMessage.Member.User.Username,
+			"discriminator": s.discordMessage.Member.User.Discriminator,
+			"discordJoinedAt": s.discordMessage.Member.JoinedAt.Format(time.RFC3339),
+			"dev": dev,
+		},
+	}
+
+	bytes, err := utils.Json.ToJson(dp)
+	if err != nil {
+		logrus.Errorf("Failed to convert data packet to json bytes: %v", err)
+		utils.ResponseHandler.WriteJSON(response, &dtos.Response{
+			Message: "Something went wrong",
+		}, http.StatusInternalServerError)
+	}
+
+	if err:= queue.SendMessage([]byte(bytes)); err != nil {
+		logrus.Errorf("Failed to send data packet to queue: %v", err)
+		utils.ResponseHandler.WriteJSON(response, &dtos.Response{
+			Message: "Something went wrong",
+		}, http.StatusInternalServerError)
+	}
+
+	messageResponse := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Your request is being processed.",
+			Flags: discordgo.MessageFlagsEphemeral,
+		},
+	}
+
+	utils.ResponseHandler.WriteJSON(response, messageResponse, http.StatusOK)
+}
--- utils/responseHandler.go
+++ utils/responseHandler.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 )
 
 type successPackage struct{}
@@ -21,3 +23,23 @@ func (Success *successPackage) NewDiscordResponse(response http.ResponseWriter, 
 		response.Write([]byte(`{"success": true, "status": 200, "message": "` + message + `"}`))
 	}
 }
+
+type responseHandler struct{}
+
+var ResponseHandler *responseHandler = &responseHandler{}
+
+func (r *responseHandler) WriteJSON(response http.ResponseWriter, data any, status int) {
+	response.Header().Set("Content-Type", "application/json")
+
+	bytes, err := Json.ToJson(data)
+	if err != nil {
+		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	response.WriteHeader(status)
+	if _, err := response.Write([]byte(bytes)); err != nil {
+		logrus.Errorf("Failed to write response: %v", err)
+	}
+}
+

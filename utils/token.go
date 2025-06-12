0673c6f9a35ab63823c99e235b7f7b8d062aad87
Pankaj <pankajshadev@gmail.com>
2025-06-12 15:31:46 +0530

    chore: remove comment and fix error message

--- utils/token.go
+++ utils/token.go
@@ -23,42 +23,32 @@ type AuthTokenHelper interface {
 type UniqueToken struct{}
 type AuthToken struct{}
 
-// GenerateUniqueToken creates a secure, unique token by hashing a combination of a UUID,
-// a cryptographically secure random number, and the current time.
 func (t *UniqueToken) GenerateUniqueToken() (string, error) {
-	// 1. Generate a new UUID
 	uuidToken := uuid.NewString()
-
-	// 2. Generate a random number up to 1,000,000
 	randNum, err := rand.Int(rand.Reader, big.NewInt(1000000))
 	if err != nil {
 		logrus.Errorf("Error generating random number: %v", err)
 		return "", fmt.Errorf("failed to generate random number: %w", err)
 	}
 
-	// 3. Get the current timestamp in milliseconds
 	generationTime := time.Now().UnixMilli()
-
-	// 4. Concatenate the parts into a single string
 	combinedString := fmt.Sprintf("%s%d%d", uuidToken, randNum, generationTime)
-	fmt.Println("Combined string: ", combinedString)
-	// 5. Calculate the SHA-256 hash
-	hasher := sha256.New()
-	hasher.Write([]byte(combinedString))
-	hashBytes := hasher.Sum(nil)
 
-	// 6. Encode the hash to a hexadecimal string
+	hasher := sha256.New()
+	if _, err := hasher.Write([]byte(combinedString)); err != nil {
+		return "", fmt.Errorf("failed to write to hasher: %w", err)
+	}
+
+	hashBytes := hasher.Sum(nil)
 	token := hex.EncodeToString(hashBytes)
-	fmt.Println("Token: ", token)
 	return token, nil
 }
 
 func (t *AuthToken) GenerateAuthToken(method jwt.SigningMethod, claims jwt.Claims, privateKey any) (string, error) {
-	logrus.Infof("Generating auth token with method: %s, claims: %v, privateKey: %v", method, claims, privateKey)
 	token := jwt.NewWithClaims(method, claims)
 	tokenString, err := token.SignedString(privateKey)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to sign token: %w", err)
 	}
 	return tokenString, nil
 }

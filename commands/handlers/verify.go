1e5f61c10ec91e15b084427269a240094318b97e
Pankaj <pankajshadev@gmail.com>
2025-06-24 17:14:28 +0530

    chore: fix formatting

--- commands/handlers/verify.go
+++ commands/handlers/verify.go
@@ -81,7 +81,7 @@ func (CS *CommandHandler) verify() error {
 		if metaData["dev"] == "true" {
 			verificationSiteURL = config.AppConfig.MAIN_SITE_URL;
 			message = createMessage("%s\n%s/discord?dev=true&token=%s\n%s")
-		}else {
+		} else {
 			verificationSiteURL = config.AppConfig.VERIFICATION_SITE_URL;
 			message = createMessage("%s\n%s/discord?token=%s\n%s")
 		}

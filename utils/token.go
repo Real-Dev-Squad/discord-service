41169a477e5abf53ffd9858788f6ec8fd407bb1f
Pankaj <pankajshadev@gmail.com>
2025-06-25 09:57:36 +0530

    feat: add helper function and test for token generation for verify command

---
+++ utils/token.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+type UniqueTokenHelper interface {
+	GenerateUniqueToken() (string, error)
+}
+type AuthTokenHelper interface {
+	GenerateAuthToken(method jwt.SigningMethod, claims jwt.Claims, privateKey any) (string, error)
+}
+
+var (
+	UniqueToken UniqueTokenHelper = &uniqueToken{}
+	AuthToken   AuthTokenHelper   = &authToken{}
+)
+
+type uniqueToken struct{}
+type authToken struct{}
+
+func (t *uniqueToken) GenerateUniqueToken() (string, error) {
+	uuidToken := uuid.NewString()
+	randNum, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		logrus.Errorf("Error generating random number: %v", err)
+		return "", fmt.Errorf("failed to generate random number: %w", err)
+	}
+
+	generationTime := time.Now().UnixMilli()
+	combinedString := fmt.Sprintf("%s%d%d", uuidToken, randNum, generationTime)
+
+	hasher := sha256.New()
+	if _, err := hasher.Write([]byte(combinedString)); err != nil {
+		return "", fmt.Errorf("failed to write to hasher: %w", err)
+	}
+
+	hashBytes := hasher.Sum(nil)
+	token := hex.EncodeToString(hashBytes)
+	return token, nil
+}
+
+func (t *authToken) GenerateAuthToken(method jwt.SigningMethod, claims jwt.Claims, privateKey any) (string, error) {
+	token := jwt.NewWithClaims(method, claims)
+	tokenString, err := token.SignedString(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+	return tokenString, nil
+}

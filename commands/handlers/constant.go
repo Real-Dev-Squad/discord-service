e4938c814bfa3a5cd5ed458e46eac34e772b3ec8
Pankaj <pankajshadev@gmail.com>
2025-06-25 10:44:34 +0530

    chore: fix nit comment

--- commands/handlers/constant.go
+++ commands/handlers/constant.go
@@ -1,19 +1,19 @@
 package handlers
 
 const(
-	DISCORD_SERVICE = "Discord Service"
-	DISCORD_AVATAR_BASE_URL = "https://cdn.discordapp.com/avatars"
-	VERIFICATION_STRING = "Please verify your discord account by clicking the link below 👇"
-	VERIFICATION_SUBSTRING = "By granting authorization, you agree to permit us to manage your server nickname displayed ONLY in the Real Dev Squad server and to sync your joining data with your user account on our platform."
+	DiscordService = "Discord Service"
+	DiscordAvatarBaseURL = "https://cdn.discordapp.com/avatars"
+	VerificationString = "Please verify your discord account by clicking the link below 👇"
+	VerificationNote = "By granting authorization, you agree to permit us to manage your server nickname displayed ONLY in the Real Dev Squad server and to sync your joining data with your user account on our platform."
 )
 
-type Request_Header struct {
+type RequestHeader struct {
 	Service string
 	Authorization string
 	ContentType string
 }
 
-var HEADERS = Request_Header {
+var DefaultHeaders = RequestHeader{
 	Service: "x-service-name",
 	Authorization: "Authorization",
 	ContentType: "Content-Type",

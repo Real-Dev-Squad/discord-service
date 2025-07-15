53726526fb790e5f9789aa77c70040f7546c9de3
Pankaj <pankajshadev@gmail.com>
2025-07-15 21:02:35 +0530

    feat: add helper function and test for token generation (#117)
    * feat: add helper function and test for token generation for verify command
    
    * chore: fix error statement and spacing
    
    * chore: fix nit comment of bot
    
    * chore: remove unnecessary error handling
    
    * refactor: fix types and remove un-necesssary logs

---
+++ utils/token.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type UniqueTokenI interface {
+	GenerateUniqueToken() (string, error)
+}
+type AuthTokenI interface {
+	GenerateAuthToken(method jwt.SigningMethod, claims jwt.Claims, privateKey any) (string, error)
+}
+
+type UniqueToken struct{}
+type AuthToken struct{}
+
+func (ut *UniqueToken) GenerateUniqueToken() (string, error) {
+	uuidToken := uuid.NewString()
+	
+	randNum, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random number: %w", err)
+	}
+
+	generationTime := time.Now().UnixMilli()
+	combinedString := fmt.Sprintf("%s%d%d", uuidToken, randNum, generationTime)
+
+	hasher := sha256.New()
+	hasher.Write([]byte(combinedString))
+	hashBytes := hasher.Sum(nil)
+	token := hex.EncodeToString(hashBytes)
+	
+	return token, nil
+}
+
+func (at *AuthToken) GenerateAuthToken(method jwt.SigningMethod, claims jwt.Claims, privateKey any) (string, error) {
+	token := jwt.NewWithClaims(method, claims)
+	
+	tokenString, err := token.SignedString(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %v", err)
+	}
+
+	return tokenString, nil
+}
---
+++ utils/token_test.go
@@ -0,0 +1,75 @@
+package utils
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"errors"
+	"testing"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+type faultyReader struct{}
+
+func (r *faultyReader) Read(p []byte) (n int, err error) {
+	return 0, errors.New("reader error")
+}
+
+func TestGenerateUniqueToken(t *testing.T) {
+	t.Run("should generate a unique token successfully", func(t *testing.T) {
+		uniqueToken := &UniqueToken{}
+		token, err := uniqueToken.GenerateUniqueToken()
+		assert.NoError(t, err)
+		assert.NotEmpty(t, token)
+		assert.Len(t, token, 64)
+	})
+
+	t.Run("should return an error when random number generation fails", func(t *testing.T) {
+		originalReader := rand.Reader
+		rand.Reader = &faultyReader{}
+		defer func() {
+			rand.Reader = originalReader
+		}()
+
+		uniqueToken := &UniqueToken{}
+		token, err := uniqueToken.GenerateUniqueToken()
+		assert.Error(t, err)
+		assert.Empty(t, token)
+		assert.Contains(t, err.Error(), "failed to generate random number")
+	})
+}
+
+func TestGenerateAuthToken(t *testing.T) {
+	t.Run("should generate an RS256 auth token successfully", func(t *testing.T) {
+		privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		assert.NoError(t, err)
+
+		name := "Discord Service"
+		claims := jwt.MapClaims{
+			"name": name,
+		}
+
+		authToken := &AuthToken{}
+		token, err := authToken.GenerateAuthToken(jwt.SigningMethodRS256, claims, privateKey)
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, token)
+
+		parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+			return &privateKey.PublicKey, nil
+		})
+		assert.NoError(t, err)
+		res := parsedToken.Claims.(jwt.MapClaims)["name"]
+		assert.Equal(t, name, res)
+	})
+
+	t.Run("should return an error for invalid key type", func(t *testing.T) {
+		claims := jwt.MapClaims{}
+		invalidKey := "<invalid-rsa-key>"
+		authToken := &AuthToken{}
+		token, err := authToken.GenerateAuthToken(jwt.SigningMethodRS256, claims, invalidKey)
+		assert.Error(t, err)
+		assert.Empty(t, token)
+	})
+}
